25bdf36dc7471737a67294250d3c8868f58148e7
IonBotaO <Dedade777>
2019-05-24 23:22:24 +0300

    Add BING.com and it passed

--- src/test/java/BingSearch.java
+++ src/test/java/BingSearch.java
@@ -1,9 +1,124 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class BingSearch {
+    WebDriver driver;
 
     //TODO: automated search scenario on BING.com
     @Test
     public void test002() {
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\TaiVanika\\IdeaProjects\\AwesomeJavaWebdriver\\src\\test\\resources\\drivers\\geckodriver.exe"); //copy path from geckodriver.exe and put it hire
+
+        driver = new FirefoxDriver();
+        // create step for testing
+        openMainPage();
+        typeQuery();
+        submitSearch();
+        waitForResults();
+        assertResultPage();
+    }
+
+    private void waitForResults () {
+        By resultStatsElement = By.className("sb_count");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(resultStatsElement));
+    }
+
+    private void assertResultPage() {
+        WebElement resultStatsElements = driver.findElement(By.className("sb_count"));
+        boolean isElementDisplayed = resultStatsElements.isDisplayed();
+        boolean expectedResult = true;
+        Assert.assertEquals(isElementDisplayed, expectedResult);
+
+        //когда запускаем код, показывает ошибку на 28-й линии, бы делает дебагинг и после чего жмём "Resume Program" она проходит нормально. Оно хотела найти информацию на сайте слишком рано, пока программа не дошла до него.
+
+    }
+
+    private void submitSearch() {
+        WebElement searchInput = driver.findElement(By.cssSelector("#sb_form_q")); //submit search google
+        searchInput.submit();
+    }
+
+    private void typeQuery() {
+        WebElement searchInput = driver.findElement(By.cssSelector("#sb_form_q")); //insert CSS Selector!!! from google/bing search (google.com/bing.com)
+        searchInput.sendKeys("portnov computer school"); //introduce search key
+    }
+
+    private void openMainPage() {
+       String url = "https://bing.com";
+       driver.get(url);
+    }
+
+}
+// FAIL line 38!!!!!!!!!!
+
+/*
+
+public class GoogleSearch {
+
+    WebDriver driver;
+
+    //TODO: run this test in DEBUGGER
+    @Test
+    public void test001() {
+
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\TaiVanika\\IdeaProjects\\AwesomeJavaWebdriver\\src\\test\\resources\\drivers\\geckodriver.exe"); //copy path from geckodriver.exe and put it hire
+
+        driver = new FirefoxDriver();
+        // create step for testing
+        openMainPage();
+        typeQuery();
+        submitSearch();
+        waitForResults();
+        assertResultPage();
+    }
+
+
+
+    private void waitForResults() {
+        By resultStatsElement = By.id("resultStats");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(resultStatsElement));
+
+    }
+
+    // TODO: add more meaningful assertion (добавить более значимое утверждение)
+    private void assertResultPage() {
+        WebElement resultStatsElements = driver.findElement(By.id("resultStats"));
+        boolean isElementDisplayed = resultStatsElements.isDisplayed();
+        boolean expectedResult = true;
+        Assert.assertEquals(isElementDisplayed, expectedResult);
+
+        //когда запускаем код, показывает ошибку на 28-й линии, бы делает дебагинг и после чего жмём "Resume Program" она проходит нормально. Оно хотела найти информацию на сайте слишком рано, пока программа не дошла до него.
+
+
+    }
+
+    private void submitSearch() {
+        WebElement searchInput = driver.findElement(By.cssSelector("html body#gsr.hp.vasq.big div#viewport.ctr-p div#searchform.jhp.big form#tsf.tsf.nj div div.A8SBwf div.RNNXgb div.SDkEP div.a4bIc input.gLFyf.gsfi")); //submit search google
+        searchInput.submit();
+    }
+
+    private void typeQuery() {
+        WebElement searchInput = driver.findElement(By.cssSelector("html body#gsr.hp.vasq.big div#viewport.ctr-p div#searchform.jhp.big form#tsf.tsf.nj div div.A8SBwf div.RNNXgb div.SDkEP div.a4bIc input.gLFyf.gsfi")); //insert Путь к CSS from google search (google.com)
+        searchInput.sendKeys("portnov computer school"); //introduce search key
+    }
+
+    private void openMainPage() {
+        String url = "https://google.com";
+
+        driver.get(url);
+
     }
 }
+
+
+// 1:44 - продебагили вход и ввод текста на google.com*/
