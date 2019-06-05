e5a8bb681fb38eb6f4753dcc0ceb8621ad6ca56b
Ion <333bax@gmail.com>
2019-06-05 23:12:15 +0300

    Day 6 Add BasicAuth, DataProviderTest, Xpath

--- src/test/java/GoogleSearch.java
+++ src/test/java/GoogleSearch.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class GoogleSearch {
@@ -12,57 +15,80 @@ public class GoogleSearch {
     WebDriver driver;
 
     //TODO: run this test in DEBUGGER
-    @Test
-    public void test001() {
+//    @Test
+//    public void test001() {
 
-
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\TaiVanika\\IdeaProjects\\AwesomeJavaWebdriver\\src\\test\\resources\\drivers\\geckodriver.exe"); //copy path from geckodriver.exe and put it hire
+    @BeforeSuite
+    public void beforeSuite() {
+    System.setProperty("webdriver.gecko.driver", "C:\\Users\\TaiVanika\\IdeaProjects\\AwesomeJavaWebdriver\\src\\test\\resources\\drivers\\geckodriver.exe"); //copy path from geckodriver.exe and put it hire
 
         driver = new FirefoxDriver();
-        // create step for testing
+    }
+
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.manage().deleteAllCookies();
+    }
+
+
+    @AfterSuite
+    public void afterSuite() {
+        driver.close();
+    }
+
+
+    //TODO: make this test to use data provider!!!!!!!!!!!!!!!!!!!!!!!!!!!  объединить 2 метода в 1. потому что у них общие составляющие
+    @Test
+    public void test001() {
+        String textValue = "portnov computer school";
+
         openMainPage();
-        typeQuery();
+        typeQuery(textValue);
         submitSearch();
         waitForResults();
         assertResultPage();
     }
 
+    @Test
+    public void test002() {
+        String textValue = "www.search4staff.com";
 
 
-    private void waitForResults() {
-        By resultStatsElement = By.id("resultStats");
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(resultStatsElement));
-
+        openMainPage();
+        typeQuery(textValue);
+        submitSearch();
+        waitForResults();
+        assertResultPage();
     }
 
-    // TODO: add more meaningful assertion (добавить более значимое утверждение)
+    private void waitForResults() {
+        By resultsStatsElement = By.id("resultStats");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(resultsStatsElement));
+    }
+
+    //TODO: add more meaningful assertion
     private void assertResultPage() {
-        WebElement resultStatsElements = driver.findElement(By.id("resultStats"));
-        boolean isElementDisplayed = resultStatsElements.isDisplayed();
+        WebElement resultsStatsElement = driver.findElement(By.id("resultStats"));
+        boolean isElementDisplayed = resultsStatsElement.isDisplayed();
         boolean expectedResult = true;
         Assert.assertEquals(isElementDisplayed, expectedResult);
-
-        //когда запускаем код, показывает ошибку на 28-й линии, бы делает дебагинг и после чего жмём "Resume Program" она проходит нормально. Оно хотела найти информацию на сайте слишком рано, пока программа не дошла до него.
-
-
     }
 
     private void submitSearch() {
-        WebElement searchInput = driver.findElement(By.cssSelector("html body#gsr.hp.vasq.big div#viewport.ctr-p div#searchform.jhp.big form#tsf.tsf.nj div div.A8SBwf div.RNNXgb div.SDkEP div.a4bIc input.gLFyf.gsfi")); //submit search google
+        WebElement searchInput = driver.findElement(By.cssSelector("#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input"));
         searchInput.submit();
     }
 
-    private void typeQuery() {
-        WebElement searchInput = driver.findElement(By.cssSelector("html body#gsr.hp.vasq.big div#viewport.ctr-p div#searchform.jhp.big form#tsf.tsf.nj div div.A8SBwf div.RNNXgb div.SDkEP div.a4bIc input.gLFyf.gsfi")); //insert Путь к CSS from google search (google.com)
-        searchInput.sendKeys("portnov computer school"); //introduce search key
+    private void typeQuery(String queryText) {
+        WebElement searchInput = driver.findElement(By.cssSelector("#tsf > div:nth-child(2) > div > div.RNNXgb > div > div.a4bIc > input"));
+        searchInput.sendKeys(queryText);
     }
 
     private void openMainPage() {
         String url = "https://google.com";
-
         driver.get(url);
-
     }
 }
 
